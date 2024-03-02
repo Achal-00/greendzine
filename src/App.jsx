b021236a1cb6d6a8f82ad952b6216257edba9864
Achal <achaldev3@gmail.com>
2024-03-03 02:40:57 +0530

    fix deployment error

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
   const [logged, setLogged] = useState(false);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/greendzine/"}>
       <Routes>
         <Route path="/" element={<Home logged={logged} />} />
         <Route path="/login" exact element={<Login setLogged={setLogged} />} />
